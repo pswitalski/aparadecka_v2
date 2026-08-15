7848b7bfaa69c75042e19f862d82a4af17654b5f
Pawel Switalski <pawelswitalski94@gmail.com>
2026-08-15 17:47:36 +0200

    feat(web): configure Sanity integration

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,6 +1,23 @@
 // @ts-check
 
+import { loadEnv } from 'vite';
 import { defineConfig } from 'astro/config';
+import sanity from '@sanity/astro';
+
+const { PUBLIC_SANITY_PROJECT_ID, PUBLIC_SANITY_DATASET } = loadEnv(
+	process.env.NODE_ENV ?? 'development',
+	process.cwd(),
+	'',
+);
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [
+		sanity({
+			projectId: PUBLIC_SANITY_PROJECT_ID,
+			dataset: PUBLIC_SANITY_DATASET,
+			apiVersion: '2026-08-15',
+			useCdn: false,
+		}),
+	],
+});
